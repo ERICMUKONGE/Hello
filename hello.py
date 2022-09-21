68e4a726e637163d6abf3621ba3453fd229aaa39
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-21 05:07:31 -0700

    First commit

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
