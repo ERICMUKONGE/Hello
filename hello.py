2cafbc17af144db937864430166ef2c604f14129
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-21 09:21:27 -0700

    First commit

--- hello.py
+++ hello.py
@@ -2,11 +2,18 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class NameForm(FlaskForm):
+    name= StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
