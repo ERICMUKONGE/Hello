67053ec21a1dea51c588176c9029a8065aa8a212
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-21 05:57:28 -0700

    First commit

--- hello.py
+++ hello.py
@@ -9,7 +9,8 @@ moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html' , current_time=datetime.utcnow())
+    return render_template('index.html' , 
+                            current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
