9c3ee36878caa6124948bbeeb9f19e6db4edf8ec
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-22 10:50:51 -0700

    First commit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -19,9 +19,13 @@ class NameForm(FlaskForm):
 def index():
     form =NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html' , form=form, name=session.get('name'))
+    return render_template('index.html' , 
+        form = form, name = session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
