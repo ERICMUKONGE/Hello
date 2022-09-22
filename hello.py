34081bbd727e2baa0edbbd3d33238d3e9c218c87
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-22 08:11:37 -0700

    First commit

--- hello.py
+++ hello.py
@@ -15,10 +15,14 @@ class NameForm(FlaskForm):
     name= StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html' , 
-                            current_time=datetime.utcnow())
+    name = None
+    form =NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html' , form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
