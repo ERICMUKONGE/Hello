c64c9b0fae3044adc25fd702b2919f8feb02131b
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-09-30 21:57:47 -0700

    First commit

--- hello.py
+++ hello.py
@@ -74,3 +74,6 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username          
 
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
