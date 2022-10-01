70a9f6f2536a49fe9d2b690fd082e2369dd4f8a0
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-10-01 00:26:58 -0700

    First commit

--- hello.py
+++ hello.py
@@ -5,16 +5,10 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-
-
+from flask_migrate import Migrate
 import os
 from flask_sqlalchemy import SQLAlchemy
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -25,6 +19,7 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+migrate = Migrate(app,db)
 
 class NameForm(FlaskForm):
     name= StringField('What is your name?', validators=[DataRequired()])
@@ -34,14 +29,20 @@ class NameForm(FlaskForm):
 def index():
     form =NameForm()
     if form.validate_on_submit():
-
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html' , 
-        form = form, name = session.get('name'))
+    return render_template('index.html',
+        form=form, name=session.get('name'),
+        known=session.get('known', False))            
 
 @app.route('/user/<name>')
 def user(name):
