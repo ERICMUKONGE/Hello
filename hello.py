2a90f421667210c698d52f087d82766c25f09208
MUKONGE ERIC <mukonge.eric@gmail.com>
2022-10-01 03:22:22 -0700

    First commit

--- hello.py
+++ hello.py
@@ -16,6 +16,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
